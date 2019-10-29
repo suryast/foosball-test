a60721d5746fd238cb633058a8eceb9b45e4598e
Surya Setiyaputra <hello@setiyaputra.me>
2019-10-30 00:48:38 +1100

    Refactor header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
+import { Typography, Box, Link, Container, Grid } from "@material-ui/core";
 
-const Link1 = React.forwardRef((props, ref) => (
+const LinkWithRoute = React.forwardRef((props, ref) => (
   <RouterLink innerRef={ref} {...props} />
 ));
 
@@ -22,12 +18,12 @@ class Header extends Component {
         </Box>
         <Grid container spacing={1}>
           <Grid item xs={6}>
-            <Link component={Link1} to="/input">
+            <Link component={LinkWithRoute} to="/input">
               Input Match Result
             </Link>
           </Grid>
           <Grid item xs={6}>
-            <Link component={Link1} to="/leaderboard">
+            <Link component={LinkWithRoute} to="/leaderboard">
               Leaderboard
             </Link>
           </Grid>
